743a8501f66f2a1b2856887b06240faa6c29fe9f
pascalhelbig <pascalhelbig@gmail.com>
2016-02-01 12:01:28 +0100

    add database connection

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://192.168.99.100/urlshortener');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('mongodb connected');
+});
+var urlSchema = mongoose.Schema({
+  url: String
+});
+var Url = mongoose.model('url', urlSchema);
+
 var app = express();
 
 app.get('/', function (req, res) {
