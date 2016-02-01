f2575a8831e2c25fa9f92a609f6b2af4d8c3141e
pascalhelbig <pascalhelbig@gmail.com>
2016-02-01 11:53:07 +0100

    add route new

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/new/:url(*)', function (req, res) {
+  res.send(req.params.url);
+});
+
 app.listen(8080, function () {
   console.log('server running on port 8080');
 });
